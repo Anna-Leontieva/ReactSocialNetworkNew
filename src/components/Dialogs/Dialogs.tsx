a4510500daaa154f066c43420b727e65b8f49859
Anna Leontieva <leontieva1anna@gmail.com>
2020-12-18 22:43:21 +0200

    add dialog componant

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
-function Dialogs(props: any) {
+import { NavLink } from 'react-router-dom';
+
+function DialogItem(props:any){
+   return(
+    <div className={classes.dialog+''+classes.active}>
+        <NavLink to={"/dialogs/"+props.id}>{props.name}</NavLink>
+    </div>
+   );
+}
+function Message(props:MessageType){
+    return(
+    <div className={classes.message}>
+      {props.message}
+    </div>
+    );
+}
+type DialogItemsType={
+    name:string
+    id:number
+}
+type MessageType={
+    message:string
+}
+function Dialogs(props: DialogItemsType) {
     return (<div className={classes.dialogs}>
-            <div className={classes.dialogItems}>
-                <div className={classes.dialog+''+classes.active}>Anna</div>
-                <div className={classes.dialog}>Dimych</div>
-                <div className={classes.dialog}>Max</div>
-                <div className={classes.dialog}>Valera</div>
-                <div className={classes.dialog}>Lena</div>
+            <div className={classes.dialogsItems}>
+                    <DialogItem id="1" name="Anna"/>
+                    <DialogItem id="2" name="Vlad"/>
+                    <DialogItem id="3" name="Lena"/>
+                    <DialogItem id="4" name="Sveta"/>
+                    <DialogItem id="5" name="Pasha"/>
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>Hi</div>
-                <div className={classes.message}>How are you?</div>
-                <div className={classes.message}>YOO!!!</div>
+                <Message message="hello"/>
+                <Message message="How are you?"/>
+                <Message message="yooo!"/>
+                <Message message="good buy"/>
             </div>
     </div>);
 }

274217f032cae35e5ec88877e148bfb59efd4abb
Anna Leontieva <leontieva1anna@gmail.com>
2020-12-27 22:36:49 +0200

    add state in index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Profile from './components/Profile/profile';
 import Dialogs from './components/Dialogs/Dialogs';
-function App(props:any) {
+function App(props:React.FC) {
   return (
   <BrowserRouter>
   <div className="app-wrapper">
     <Header/>
     <Navbar/>
     <div className="app-wrapper-content">
-      <Route path='/dialogs' component={Dialogs}/>
-      <Route path='/profile' component={Profile}/>
+      <Route path='/dialogs' render={()=><Dialogs/>} dialogs={props.dialogs} messages={props.messages}/>
+        <Route path='/profile' render={()=><Profile/>} posts={props.posts}/>
     </div>
     </div>
   </BrowserRouter>

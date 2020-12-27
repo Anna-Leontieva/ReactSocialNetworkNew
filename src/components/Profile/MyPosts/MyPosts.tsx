c620805eff14dc87047dd16fd75656d6220c0d18
Anna Leontieva <leontieva1anna@gmail.com>
2020-12-27 18:22:31 +0200

    change dialogs and posts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
-function MyPosts(props:any) {
+function MyPosts() {
+    let posts=[
+        {id:1,message:"Hi,how are you?",likeCounts:44},
+        {id:1,message:"It my first post",likeCounts:344}
+    ]
+    let postsElements=posts.map(m=><Post message={m.message} likeCounts={m.likeCounts}/>)
     return (
         <div>
             <div>
@@ -13,8 +18,7 @@ function MyPosts(props:any) {
                 <button>Add post</button>
             </div>
             <div className={classes.posts}>
-             <Post message="Hi,how are you?" likeCounts={44}/>
-             <Post message="IT my first post" likeCounts={334}/>
+          {postsElements}
             </div>
         </div>
     );
